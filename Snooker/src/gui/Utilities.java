99a87a03fe20aa15fe45e44b6036bb81dfeb83bc
botas222 <pereiraffjoao1993@gmail.com>
2014-06-06 01:51:57 +0100

    teste

--- Snooker/src/gui/Utilities.java
+++ Snooker/src/gui/Utilities.java
@@ -5,12 +5,8 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 
 public class Utilities {
-<<<<<<< HEAD
-	//ppublic static Dimension dimScreen = Toolkit.getDefaultToolkit().getScreenSize();
 	public static Dimension dimScreen = new Dimension(1200, 800);
-=======
-	public static Dimension dimScreen = new Dimension(1280, 800);
->>>>>>> 42f051ea8d88b388b9e02735d80fdc9fadf60b94
+	
 	public static Dimension optionsMenuSize = new Dimension(600, 600);
 	public static Color[] ballsColors = { Color.WHITE, Color.RED.darker(), Color.YELLOW.darker(),
 			Color.GREEN, new Color(150, 75, 0), Color.BLUE.darker(),
