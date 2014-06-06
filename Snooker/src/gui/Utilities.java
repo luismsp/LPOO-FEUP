42f051ea8d88b388b9e02735d80fdc9fadf60b94
kii407 <luispina407@gmail.com>
2014-06-06 01:48:58 +0100

    fixed dimension

--- Snooker/src/gui/Utilities.java
+++ Snooker/src/gui/Utilities.java
@@ -5,7 +5,7 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 
 public class Utilities {
-	public static Dimension dimScreen = new Dimension(1200, 800);
+	public static Dimension dimScreen = new Dimension(1280, 800);
 	public static Dimension optionsMenuSize = new Dimension(600, 600);
 	public static Color[] ballsColors = { Color.WHITE, Color.RED.darker(), Color.YELLOW.darker(),
 			Color.GREEN, new Color(150, 75, 0), Color.BLUE.darker(),
